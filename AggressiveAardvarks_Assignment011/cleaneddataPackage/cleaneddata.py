871a1e0cd78056e078f085dddc89f509b1e989eb
Ben Ujvagi <ujvagibw@mail.uc.edu>
2024-11-18 18:09:31 -0500

    Add project files.

---
+++ AggressiveAardvarks_Assignment011/cleaneddataPackage/cleaneddata.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+class DuplicateCleaner:
+    
+     def save_cleaned_data(self, file_path): self.data.to_csv(file_path, index=False)
---
+++ AggressiveAardvarks_Assignment011/duplicatesPackage/duplicates.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+class DuplicateCleaner:
+
+    def remove_duplicates(self): self.data.drop_duplicates(inplace=True) 
